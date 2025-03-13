cc7444773e1bf6286b7986ff648b0935907deff5
speedwagon1299 <srihari5202004@gmail.com>
2025-03-14 00:00:26 +0530

    More API stuff

--- Concepts/APICall/template.py
+++ Concepts/APICall/template.py
@@ -11,38 +11,39 @@ sys_instruct = """
                 embedded in microsoft/graphcodebert-base.
             """
 
+"""
+You will be given a stack trace of an error and its associated code snippet in python. If the error is simple and solvable by your current knowledge without documentation, return "DocRequired" with False and the other paramaters with NULL. Else, you have to name the required library's documentation as an enum in "Library" and generate an upto 8 word phrase without need for punctuation and semantics which will be useful when performing a similarity search in FAISS vector store embedded in microsoft/graphcodebert-base in the parameter "SearchPhrase".
+"""
+
 user_prompt = """
                 {
-                    "error_point": {
-                        "file": "C:\\Users\\sriha\\OneDrive\\Desktop\\Projects\\RAG\\StackOverFix\\tests\\test_extractor.py",
-                        "line": 19,
-                        "function": "recursive_function",
-                        "code": "return recursive_function()  # Stack Overflow Error"
-                    },
-                    "first_site_package_error": null,
-                    "filtered_trace": [
-                        {
-                            "file": "C:\\Users\\sriha\\OneDrive\\Desktop\\Projects\\RAG\\StackOverFix\\tests\\test_extractor.py",
-                            "line": 23,
-                            "function": "test_stack_overflow_error",
-                            "code": "recursive_function()"
-                        },
-                        {
+                    "user_prompt": "Fix the recursion error here",
+                    "code_snippet": "def test_stack_overflow_error():\n    try:\n        recursive_function()\n    except RecursionError as e:\n        error_details = extract_stack_trace(e)",
+                    "stack_trace": {
+                        "exception": "RecursionError",
+                        "message": "maximum recursion depth exceeded",
+                        "error_point": {
                             "file": "C:\\Users\\sriha\\OneDrive\\Desktop\\Projects\\RAG\\StackOverFix\\tests\\test_extractor.py",
                             "line": 19,
                             "function": "recursive_function",
                             "code": "return recursive_function()  # Stack Overflow Error"
                         },
-                        {
-                            "file": "C:\\Users\\sriha\\OneDrive\\Desktop\\Projects\\RAG\\StackOverFix\\tests\\test_extractor.py",
-                            "line": 19,
-                            "function": "recursive_function",
-                            "code": "return recursive_function()  # Stack Overflow Error"
-                        }
-                    ],
-                    "exception": "RecursionError",
-                    "message": "maximum recursion depth exceeded"
-                } 
+                        "filtered_trace": [
+                            {
+                                "file": "C:\\Users\\sriha\\OneDrive\\Desktop\\Projects\\RAG\\StackOverFix\\tests\\test_extractor.py",
+                                "line": 23,
+                                "function": "test_stack_overflow_error",
+                                "code": "recursive_function()"
+                            },
+                            {
+                                "file": "C:\\Users\\sriha\\OneDrive\\Desktop\\Projects\\RAG\\StackOverFix\\tests\\test_extractor.py",
+                                "line": 19,
+                                "function": "recursive_function",
+                                "code": "return recursive_function()  # Stack Overflow Error"
+                            }
+                        ]
+                    }
+                }
             """
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 1. User writes python code with a python library and gets some exception
 2. Exception is caught and passed through stackoverfix package which attaches the error stack trace to the user's clipboard
-3. In the designated prompgitt areas, user pastes the stack trace in a textbox
+3. In the designated prompt areas, user pastes the stack trace in a textbox
 4. User attaches the snippet area of interest as well separately in a textbox
 5. First API call is to evaluate whether documentation is required or not to generate an appropriate search phrase to find the corresponding error in the documentation through faiss. If required, then phrase is provided, else FALSE is returned. If FALSE, send information to Gemini api and retrieve generated text solving the error
 6. If phrase is provided, search the vector store and retrieve top 2 results
